eca156a48faba4312651d97dff0c5f68181f4675
Lucas <lucas_fasael@hotmail.com>
2022-11-22 21:36:14 -0300

    variaveis, parse, conversão virgula e localidade

---
+++ JavaScript/guanabara/ex2/ex2.js
@@ -0,0 +1,4 @@
+//window.alert("Minha primeira mensagem!")
+//window.confirm("Está gostando de JS ?")
+var nome = window.prompt("Qual é o seu nome ? ")
+window.alert("É um prazer te conhecer, " + nome + "!" )
